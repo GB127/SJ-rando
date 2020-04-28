9a3b37f05f41ada2bf932deb8473221f4aa40a0d
Guylain <guylain.breton.1@ulaval.ca>
2020-04-28 13:26:19 -0400

    More works. Preparing for first test

--- rando.py
+++ rando.py
@@ -2,15 +2,12 @@ from gameclass import ROM, infos
 import random
 infos = infos()
 
-"""
-with open("ROM.nes", "rb") as original:
-    originaldata = original.read()
-    game = ROM(originaldata)
-"""
 infos.listadresses("palettes")
 
 
-def paletterandomizer():
+def paletterandomizer(game):
+    #Create the pool of colors
+        #FIXME : I'm sure it can be improved. But for the time being it will be like that
     colors = list(range(0x0, 0x0E))
     colors += list(range(0x10, 0x1E))
     colors += list(range(0x20, 0x2E))
@@ -19,11 +16,20 @@ def paletterandomizer():
     colors.remove(0x1D)
 
     # Rocket palette
+        # It's split into 3 variables since I plan to do operations so the randomized palettes aren't too crazy.
     maincolor = random.choice(colors)
     secondary = random.choice(colors)
     window = random.choice(colors)
-
     print(maincolor, secondary,window)
+    game[0x75D6] = maincolor
+    game[0x75E1] = maincolor
+    game[0x75D8] = secondary
+    game[0x75D6] = window
 
 
-paletterandomizer()
+with open("ROM.nes", "rb") as original:
+    originaldata = original.read()
+    randogame = ROM(originaldata)
+    paletterandomizer(randogame)
+    with open("ROM2.nes", "wb") as newrom:
+        newrom.write(randogame.data)
