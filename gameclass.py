394402e1550a5d12bb2ba08e2f9042c92c39f6d0
Guylain <guylain.breton.1@ulaval.ca>
2020-04-30 10:13:41 -0400

    Remove what I'll change

--- gameclass.py
+++ gameclass.py
@@ -9,6 +9,14 @@ class infos:
         Retrieve the things I need.
     """
     def __init__(self):
+        # List of infos:
+            # palette
+            # pos
+                # Xpos
+                # Ypos
+            # timer
+            # Rocket
+
         self.infos = {
                         hex(0x75D6)  : "Rocket window palette" ,
                         hex(0x75D7)  : "Rocket main palette",
@@ -16,34 +24,24 @@ class infos:
                         hex(0x75D8)  : "Rocket sec palette",
                         hex(0x75E2)  : "Score palette",
                         hex(0x75E3)  : "Bars palette",
-                        hex(0x753C)  : "Palette intro level",  # FIXME
-                        hex(0x753D)  : "Palette intro level", # FIXME
-                        hex(0x753E)  : "Palette intro level", # FIXME
-                        hex(0x75AC)  : "Dot palette of Title screen",
+                        hex(0x753C)  : "palette intro level",  # FIXME
+                        hex(0x753D)  : "palette intro level", # FIXME
+                        hex(0x753E)  : "palette intro level", # FIXME
+                        hex(0x75AC)  : "palette of dot Title screen",
                         hex(0x99)    : "Current Wall invicibility timer",
                         hex(0x011011): "Wall Invicibility timer Duration",
                         hex(0x0386B7): "Rocket fuel consumption on press A",
-                        hex(0xC6)    : "Current fuel (2B)",
-                        hex(0x0374)  : "Direction of the rocket",
-                        hex(0x4C)    : "Camera X position",
-                        hex(0x4E)    : "Camera Y position",
+                        hex(0xC6)    : "Rocket Current fuel (2B)",
+                        hex(0x0374)  : "Direction of the Rocket",
+                        hex(0x4C)    : "Camera Xpos",
+                        hex(0x4E)    : "Camera Ypos",
                         hex(0x5D)    : "Level timer",
-                        hex(0x0200)  : "X pos",
-                        hex(0x021F)  : "Xpos * 256",
-                        hex(0x0197)  : "subXpos",
-                        hex(0x029B)  :  "X speed",
-                        hex(0x02BA)  :    "Xspeed *256"
+                        hex(0x0200)  : "Rocket Xpos",
+                        hex(0x021F)  : "Rocket Xpos * 256",
+                        hex(0x0197)  : "Rocket subXpos",
+                        hex(0x029B)  : "Rocket Xpos speed",
+                        hex(0x02BA)  : "Rocket Xpos speed *256"
                     }
-        self.categories ={
-            "palettes" : [hex(0x75D6), hex(0x75D7),hex(0x75E1),
-                          hex(0x75D8), hex(0x75E2),hex(0x75E3),
-                          hex(0x75AC), hex(0x753C),hex(0x753D),hex(0x753E)]
-            ,"Rocket mob" : [hex(0x011011), hex(0x0386B7), hex(0x0374)]
-                        
-            ,"infos" : [hex(0x99), hex(0xC6), hex(0x4C), hex(0x4E), hex(0x5D)],
-
-            "xpos" : [hex(0x200), hex(0x021F), hex(0x029B), hex(0x029A)]
-}
     def check(self,adress):
         if isinstance(adress,str):
             return self.infos[adress]
@@ -55,9 +53,9 @@ class infos:
         print(self.check(adress))
 
     def listadresses(self,key):
+        # FIXME : Do some code here!
         print("------------------------------")
-        for adress in self.categories[key]:
-            print(adress, " : ",  self.check(adress))
+        print(" : ",  self.check())
         print("------------------------------")
 
 
