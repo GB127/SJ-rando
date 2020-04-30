70092e5ca2ee108d7eb794930a565b8b4969dc93
Guylain <guylain.breton.1@ulaval.ca>
2020-04-30 10:08:39 -0400

    Add some infos

--- gameclass.py
+++ gameclass.py
@@ -10,23 +10,40 @@ class infos:
     """
     def __init__(self):
         self.infos = {
-                        hex(0x75D6) : "Rocket window palette" ,
-                        hex(0x75D7) : "Rocket main palette",
-                        hex(0x75E1) : "Rocket icon palette",
-                        hex(0x75D8) : "Rocket sec palette",
-                        hex(0x75E2) : "Score palette",
-                        hex(0x75E3) : "Bars palette",
-                        hex(0x753C) : "Palette intro level",  # FIXME
-                        hex(0x753D) : "Palette intro level", # FIXME
-                        hex(0x753E) : "Palette intro level", # FIXME
-                        hex(0x75AC) : "Dot palette of Title screen",
-                        hex(0x99) : "Wall invicibility timer"
+                        hex(0x75D6)  : "Rocket window palette" ,
+                        hex(0x75D7)  : "Rocket main palette",
+                        hex(0x75E1)  : "Rocket icon palette",
+                        hex(0x75D8)  : "Rocket sec palette",
+                        hex(0x75E2)  : "Score palette",
+                        hex(0x75E3)  : "Bars palette",
+                        hex(0x753C)  : "Palette intro level",  # FIXME
+                        hex(0x753D)  : "Palette intro level", # FIXME
+                        hex(0x753E)  : "Palette intro level", # FIXME
+                        hex(0x75AC)  : "Dot palette of Title screen",
+                        hex(0x99)    : "Current Wall invicibility timer",
+                        hex(0x011011): "Wall Invicibility timer Duration",
+                        hex(0x0386B7): "Rocket fuel consumption on press A",
+                        hex(0xC6)    : "Current fuel (2B)",
+                        hex(0x0374)  : "Direction of the rocket",
+                        hex(0x4C)    : "Camera X position",
+                        hex(0x4E)    : "Camera Y position",
+                        hex(0x5D)    : "Level timer",
+                        hex(0x0200)  : "X pos",
+                        hex(0x021F)  : "Xpos * 256",
+                        hex(0x0197)  : "subXpos",
+                        hex(0x029B)  :  "X speed",
+                        hex(0x02BA)  :    "Xspeed *256"
                     }
-        self.categories ={"palettes" : [hex(0x75D6), hex(0x75D7),hex(0x75E1),
-                                        hex(0x75D8), hex(0x75E2),hex(0x75E3),
-                                        hex(0x75AC), hex(0x753C),hex(0x753D),hex(0x753E)]
-                        }
+        self.categories ={
+            "palettes" : [hex(0x75D6), hex(0x75D7),hex(0x75E1),
+                          hex(0x75D8), hex(0x75E2),hex(0x75E3),
+                          hex(0x75AC), hex(0x753C),hex(0x753D),hex(0x753E)]
+            ,"Rocket mob" : [hex(0x011011), hex(0x0386B7), hex(0x0374)]
+                        
+            ,"infos" : [hex(0x99), hex(0xC6), hex(0x4C), hex(0x4E), hex(0x5D)],
 
+            "xpos" : [hex(0x200), hex(0x021F), hex(0x029B), hex(0x029A)]
+}
     def check(self,adress):
         if isinstance(adress,str):
             return self.infos[adress]
