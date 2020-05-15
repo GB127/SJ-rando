441be495f486c12b45c3b98062c0e780240ead64
Guylain <guylain.breton.1@ulaval.ca>
2020-05-14 21:42:33 -0400

    Command line analyser!

--- main.py
+++ main.py
@@ -5,26 +5,26 @@ from rando import *
 def getoptions():
     parser = argparse.ArgumentParser(description='Solar Jet Randomizer : Randomizers selected')
     parser.add_argument('-p', "--palette", action="store_true",
-                    help='Randomize the colors', metavar="PaletteRando")
+                    help='Randomize the colors', dest="PaletteRando")
     parser.add_argument('-e', "--engine", action="store_true",
-                    help='Randomize the engine', metavar="EngineRando")
+                    help='Randomize the engine', dest="EngineRando")
     parser.add_argument('-m', "--management", action="store_true",
-                    help='Randomize the management', metavar="ManagementRando")
+                    help='Randomize the management', dest="ManagementRando")
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     options = getoptions()
-    with open("ROM.nes", "rb") as original:
+    with open("Vanilla.nes", "rb") as original:
         originaldata = original.read()
         randogame = ROM(originaldata)
-        if options.PaleteRando == True:
+        if options.PaletteRando == True:
             paletterandomizer(randogame)
         if options.EngineRando == True:
             engine_randomizer(randogame)
         if options.ManagementRando == True:
             manage_randomizer(randogame)
-        with open("ROM2.nes", "wb") as newrom:
+        with open("testing.nes", "wb") as newrom:
             newrom.write(randogame.data)
 
 
