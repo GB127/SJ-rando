be71789fa4fb5555c223d90d9718bb16b64e0977
Guylain <guylain.breton.1@ulaval.ca>
2020-05-15 13:42:19 -0400

    Rando AccelX!

--- rando.py
+++ rando.py
@@ -3,9 +3,6 @@ import random
 
 def paletterandomizer(game):
     #Create the pool of colors
-        #FIXME : I'm sure it can be improved. But for the time being it will be like that
-            # idea 1 : create a for _ ion range 4
-            # idea 2 : create the entire palette 90 to EF or somethimg, then remove the colors with a loop
     colors = list(range(0x1, 0x0D))
     colors += list(range(0x11, 0x1D))
     colors += list(range(0x21, 0x2D))
@@ -29,17 +26,41 @@ def engine_randomizer(game):
             - Speed max of the rocket
             - Gravity
     """
+    print("Speed rando!")
     # Randomize speedmaxs:
         # The original value is 3, which is slow IMO.
         # So the randomizer will randomize to something higher.
         # The goal is to have a worst case scenario of vanilla feature,
         # and a "best case scenario" where the speed is very high.
-        # The middle will be what I consider to be a perfect value for casual play
     game[0x048D3] = random.randint(3,12)  # both speed
     game[0x3b8a3] = game[0x48D3]  # x speed
     game[0x3b8ac] = game[0x48D3]  # x speed
     game[0x3b909] = game[0x48D3]  # y speed
     game[0x3b912] = game[0x48D3]  # y speed
+    print("Accelrando!")
+    # Randomize acceleration!
+        # The acceleration depends on the rocket direction and each
+        # rocket's acceleration are stored independently.
+        # So I'm going to alter all of them.
+        # Approach:
+            # First, Either flip the acceleration table, linearize it or keep it.
+    delta_acceleration = [12,12,11,10,8,6,3,2]
+    reversaccel = list(reversed(delta_acceleration))
+    linearized = [8,8,8,8,8,8,8,8]
+    rando_delta_accel = random.choice([delta_acceleration,reversaccel,linearized])
+    randoaccel = []
+    for i in range(len(rando_delta_accel)):
+        randoaccel.append(sum(rando_delta_accel[:i+1]))
+    game[0x38866] = random.randint(randoaccel[-1], 255)   # This is the max accel. Wrote it ike that in case I change something
+    ratio = game[0x38866] / 64
+    game[0x38867] = int(randoaccel[-2] * ratio)
+    game[0x38868] = int(randoaccel[-3] * ratio)
+    game[0x38869] = int(randoaccel[-4] * ratio)
+    game[0x3886A] = int(randoaccel[-5] * ratio)
+    game[0x3886B] = int(randoaccel[-6] * ratio)
+    game[0x3886C] = int(randoaccel[-7] * ratio)
+    game[0x3886D] = int(randoaccel[-8] * ratio)
+    game[0x3886E] = 0 # I don't change this, but just to "Know that it's considered" I'm writting it down.
 
 
 def manage_randomizer(game):
@@ -54,8 +75,8 @@ def manage_randomizer(game):
         # Default value is 2, which is abysmal IMO. So let's crank it a big notch :D
         # The possible range is 0 (yes yes, free fuel!) up to 254
         # The double thruster is always higher by at least one.
-    game[0x386B7] = random.randint(0,254)
-    game[0x386AF] = random.randint(game[0x386B7] +1, 255)
+    game[0x386B7] = random.randint(0,254)  # Normal
+    game[0x386AF] = random.randint(game[0x386B7] +1, 255)  # Thruster : I'll have to think on this.
 
     # This is the starting fuel.
         # Default value is E0 for 224 * 255 unit.
@@ -77,4 +98,4 @@ def manage_randomizer(game):
 
 if __name__ == "__main__" :
     infos = infos()
-    infos.listadresses("fuel")
+    infos.listadresses("acceleration")
