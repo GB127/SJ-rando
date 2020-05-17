ff3e48afd0acdf325d02fe068bc3414526c0886b
Guylain <guylain.breton.1@ulaval.ca>
2020-05-17 18:48:30 -0400

    Add one cheat

--- qol.py
+++ qol.py
@@ -5,3 +5,9 @@ def removeohko():
     game[0x008404] = 234
     game[0x008405] = 234
     game[0x008406] = 234
+
+def invicible_collisions():
+    game[0x11030] = 234
+    game[0x11031] = 234
+    game[0x11036] = 234
+    game[0x11037] = 234
