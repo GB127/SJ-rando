95884598419f1d053074f11d032f6916c76b1c2b
Guylain <guylain.breton.1@ulaval.ca>
2020-05-22 18:13:46 -0400

    oops

--- qol.py
+++ qol.py
@@ -1,20 +1,20 @@
 from gameclass import ROM, infos
 
-def removeohko(game):
+def disable_ohko(game):
     game[0x008403] = 234
     game[0x008404] = 234
     game[0x008405] = 234
     game[0x008406] = 234
 
-def invicible_collisions(game):
+def disable_fuelloss_collisions(game):
     game[0x11030] = 234
     game[0x11031] = 234
     game[0x11036] = 234
     game[0x11037] = 234
 
 def disable_shield_fuelusage(game):
-    game[0x0049f0] = 234
-    game[0x0049f1] = 234
+    game[0x0049f0] = 0
+    game[0x0049f1] = 0
 
 def disable_fuelusage(game):
     game[0x386B7] = 0  # Normal
