21b99534cd58958c1184ea65dd03043c84fa5a3d
Guylain <guylain.breton.1@ulaval.ca>
2020-05-26 12:53:11 -0400

    Disable the spring effect!

--- qol.py
+++ qol.py
@@ -19,3 +19,20 @@ def disable_shield_fuelusage(game):
 def disable_fuelusage(game):
     game[0x386B7] = 0  # Normal
     game[0x386AF] = 0
+
+
+
+
+
+
+
+def disable_springeffect(game):
+    game[0x48A1] = 0xEA
+    game[0x48A2] = 0xEA
+    game[0x48A3] = 0xEA  # NOP au lieu de SEC (38 vanilla)
+    game[0x48A4] = 0xEA
+    game[0x48A5] = 0xEA
+    game[0x48A6] = 0xEA
+    game[0x48A7] = 0xEA
+    game[0x48A8] = 0xEA
+    game[0x48A9] = 0xEA
