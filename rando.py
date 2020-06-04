9c0cfeaeb021586778d25de3f123115e11872cd2
Guylain <guylain.breton.1@ulaval.ca>
2020-06-04 09:10:20 -0400

    Reorder the functions alphabetically

--- rando.py
+++ rando.py
@@ -2,32 +2,6 @@ from gameclass import ROM, infos
 from subrando import nescolors, getnewcolor, getdistributionaccel, getwarheadaccel, planetitem_shuffler
 import random
 
-def palette_randomizer(game, seed):
-    random.seed(seed)
-    colors = nescolors()
-    #Create the pool of colors
-
-    # HUD display
-    game[0x75E2] = random.choice(colors)  # Score palette
-    game[0x753C] = game[0x75E2]  # Score palette too
-    game[0x75E3] = random.choice(colors)  # Bars palette
-    game[0x75ac] = random.choice(colors)  # Title screen dots
-    # Rocket palette
-    game[0x75D8] = random.choice(colors[0:27])  # Main color
-    game[0x75D7] = colors[getnewcolor(colors.index(game[0x75D8]), colors)]  # secondary color
-    game[0x75E1] = game[0x75D8] # Icon color is the same as the main color
-    game[0x00753B] = game[0x75D8]
-    # game[0x75D6] = random.choice(colors)  # window color
-
-    game[0x0075ad] = random.choice(colors) # C'est le milieu
-    game[0x0075aE] = random.choice(colors)  # C'est l'autre
-
-def item_randomizer(game,seed):
-    random.seed(seed)
-    pass
-
-
-
 
 
 def astro_randomizer(game,seed):
@@ -55,6 +29,43 @@ def astro_randomizer(game,seed):
     game[0x3a98] = game[0x3a8f]  # Fastfall maxspeed Y
 
 
+def fuel_randomizer(game, seed):
+    """ 
+        This function will randomize the following stuffs:
+            - Fuel related stuffs
+            - Invicibility timer
+    """
+    random.seed(seed)
+
+    # This is for the fuel consumption.
+        # On press A
+        # Default value is 2, which is abysmal IMO. So let's crank it a big notch :D
+        # The possible range is 0 (free fuel!) up to 254
+        # The double thruster is always higher by at least one.
+    game[0x386B7] = random.randint(0,254)  # Normal
+    game[0x386AF] = random.randint(game[0x386B7], 255)  # Thruster
+
+    # This is the starting fuel.
+        # Default value is E0 for 224 * 255 unit.
+        # In other words, you start with 57 120 fuel!
+        # this is a big number. So let's crank it down, shall we?
+        # The goal is to have a minimum where you have to not use fuel
+        # and use momentum to travel to save on fuel or to find and use fuel tanks.
+    game[0x7202] = random.randint(1,255)  # Home
+    game[0x8589] = random.randint(game[0x7202], 255)  # Fuel tanks
+        # So it will always either give you more health
+        # or give the same max health
+
+
+
+    # This is for the invicibility timer on wall collision.
+        # Default value is 30 (28 in decimal).
+        # This game run 30 FPS per seconds I think. So you have 1 second of invicibility.
+        # Let's alter this a little bit.
+        # 1 being no invicibility at all
+        # 255 being the max value of a byte
+    game[0x11011] = random.randint(1,255)
+
 
 def gravity_randomizer(game,seed, test):
     gravity = test# random.randint(0,255)  # FIXME
@@ -62,6 +73,33 @@ def gravity_randomizer(game,seed, test):
     game[0x01040E] = gravity # This is the operand. Normally it's F8.
     game[0x0107B3] = 169
     game[0x0107B4] = gravity
+
+
+def item_randomizer(game,seed):
+    random.seed(seed)
+    pass
+
+
+def palette_randomizer(game, seed):
+    random.seed(seed)
+    colors = nescolors()
+    #Create the pool of colors
+
+    # HUD display
+    game[0x75E2] = random.choice(colors)  # Score palette
+    game[0x753C] = game[0x75E2]  # Score palette too
+    game[0x75E3] = random.choice(colors)  # Bars palette
+    game[0x75ac] = random.choice(colors)  # Title screen dots
+    # Rocket palette
+    game[0x75D8] = random.choice(colors[0:27])  # Main color
+    game[0x75D7] = colors[getnewcolor(colors.index(game[0x75D8]), colors)]  # secondary color
+    game[0x75E1] = game[0x75D8] # Icon color is the same as the main color
+    game[0x00753B] = game[0x75D8]
+    # game[0x75D6] = random.choice(colors)  # window color
+
+    game[0x0075ad] = random.choice(colors) # C'est le milieu
+    game[0x0075aE] = random.choice(colors)  # C'est l'autre
+
 
 def rocket_randomizer(game, seed):
     """
@@ -107,45 +145,6 @@ def rocket_randomizer(game, seed):
     game[0x3886E] = 0 # I don't change this, but just to "Know that it's considered" I'm writting it down.
 
 
-def fuel_randomizer(game, seed):
-    """ 
-        This function will randomize the following stuffs:
-            - Fuel related stuffs
-            - Invicibility timer
-    """
-    random.seed(seed)
-
-    # This is for the fuel consumption.
-        # On press A
-        # Default value is 2, which is abysmal IMO. So let's crank it a big notch :D
-        # The possible range is 0 (free fuel!) up to 254
-        # The double thruster is always higher by at least one.
-    game[0x386B7] = random.randint(0,254)  # Normal
-    game[0x386AF] = random.randint(game[0x386B7], 255)  # Thruster
-
-    # This is the starting fuel.
-        # Default value is E0 for 224 * 255 unit.
-        # In other words, you start with 57 120 fuel!
-        # this is a big number. So let's crank it down, shall we?
-        # The goal is to have a minimum where you have to not use fuel
-        # and use momentum to travel to save on fuel or to find and use fuel tanks.
-    game[0x7202] = random.randint(1,255)  # Home
-    game[0x8589] = random.randint(game[0x7202], 255)  # Fuel tanks
-        # So it will always either give you more health
-        # or give the same max health
-
-
-
-    # This is for the invicibility timer on wall collision.
-        # Default value is 30 (28 in decimal).
-        # This game run 30 FPS per seconds I think. So you have 1 second of invicibility.
-        # Let's alter this a little bit.
-        # 1 being no invicibility at all
-        # 255 being the max value of a byte
-    game[0x11011] = random.randint(1,255)
-
-
-
 def weapon_randomizer(game, seed):
     random.seed(seed)
     # Smart bombs:
@@ -185,7 +184,6 @@ def weapon_randomizer(game, seed):
     game[0x03c3c7] = game[0x03c3c3]  # 14
     game[0x03c3c8] = game[0x03c3c4]  # 20
     game[0x03c3c9] = game[0x03c3c3]  # 14
-
 
 
 if __name__ == "__main__" :
