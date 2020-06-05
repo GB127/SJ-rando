fc6fad9ea90a1c937a484f6170aa043b92808b40
Guylain <guylain.breton.1@ulaval.ca>
2020-06-04 21:46:40 -0400

    Reorder stuffs and disable incomplete randos

--- main.py
+++ main.py
@@ -4,22 +4,23 @@ from qol import *
 
 def getoptions():
     parser = argparse.ArgumentParser(description='Solar Jet Randomizer : Randomizers selected')
-    parser.add_argument("--seed", action="store", help="Seed for the randomization",
-                        dest="seed", default=random.random(), metavar="")
-    parser.add_argument('-p', "--palette", action="store_true",
-                        help='Randomize the colors', dest="Rpalette")
     parser.add_argument("-a", "--astro", action="store_true", 
                         help="Randomize the Astronaut's properties", dest="Rastro")
     #parser.add_argument("-g", "--gravity", action="store_true",
                         #help="Randomize the gravity", dest="Rgrav"),
+    parser.add_argument('-p', "--palette", action="store_true",
+                        help='Randomize the colors', dest="Rpalette")
     parser.add_argument("-r", "--rocket", action="store_true",
                         help="Randomize the pod's properties", dest="Rrocket")
-    parser.add_argument("-f","--fuel", action="store_true",
-                        help="Randomize things related to the fuel", dest="Rfuel")
-    parser.add_argument("-w", "--weapon", action="store_true",
-                        help="Randomize the weapons' properties", dest="Rweapon")
+    #parser.add_argument("-f","--fuel", action="store_true",
+    #                    help="Randomize things related to the fuel", dest="Rfuel")
+    #parser.add_argument("-w", "--weapon", action="store_true",
+    #                    help="Randomize the weapons' properties", dest="Rweapon")
+    parser.add_argument("--seed", action="store", help="Seed for the randomization",
+                        dest="seed", default=random.random(), metavar="")
     parser.add_argument("--mode", choices=[None, "reckless", "improved"],
                         default=None, dest="mode", help="Game mode")
+
     return parser.parse_args()
 
 
@@ -29,6 +30,7 @@ if __name__ == "__main__":
     with open("Vanilla.nes", "rb") as original:
         originaldata = original.read()
         randogame = ROM(originaldata)
+        remove_uselesscodes(randogame)
         if options.Rpalette:
             palette_randomizer(randogame, seed)
         if options.Rastro:
