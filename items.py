fec31d235e3a0ef6a79c3f615d36587199067009
Guylain <guylain.breton.1@ulaval.ca>
2020-06-07 15:30:48 -0400

    Finally, all items are there and displayed nicely

--- items.py
+++ items.py
@@ -44,14 +44,52 @@ planetitems ={
            fuel,        fuel,       fuel,       points2,    points2,
            radio,       radio,      fueltank,   warp,       warp, 
            fueltank,    fueltank,   life,       life],
-    "6" : [gwarp, crystal, crystal2, fuel, crystal2, crystal2, fuel, fuel, crystal2, fuel, points2, points2, radio, radio, radio, radio, fueltank, warp, warp, warp, warp, life, life, life],
-    "7" : [gwarp, crystal2, crystal2, fuel, crystal2, fuel, crystal2, fuel, crystal2, fuel, crystal, warp, warp, points2, points2, radio, radio, fueltank, fueltank, warp, life, fueltank, life, life],
-    "8" : [minigame, gwarp, crystal2, crystal2, fuel, crystal, fuel, fuel, fuel, crystal2, radio, points2, radio, radio, warp, warp, fueltank, warp, fueltank, fueltank, life, life],
-    "9" : [gwarp, minigame, fuel, crystal2, fuel, crystal2, fuel, crystal2, fuel, fuel, crystal, points2, points2, radio, points2, warp, fueltank, fueltank, warp, warp, fueltank, life],
-    "10" : [gwarp, crystal2, crystal, fuel, fuel, crystal2, fuel, fuel, crystal2, fuel, crystal2, points2, points2, radio, points2, life, warp, fueltank, life, warp, ],
-    "11" : [gwarp, crystal2, crystal2, fuel, fuel, crystal, crystal2, fuel, crystal2, fuel, crystal2, points2, points2, radio, points2, radio, warp, fueltank, warp, warp, fueltank, fueltank, life],
-    "12" : [gwarp, crystal2, crystal2, fuel, crystal2, crystal2, fuel, crystal, fuel, fuel, crystal2, points2, points2, radio, points2, points2, warp, warp, fueltank, warp, warp, fueltank, life, life],
-    "13" : [gwarp, crystal2, fuel, crystal, crystal2, fuel, fuel, crystal2, fuel, points2, points2, radio, radio, fueltank, warp, warp, warp, life, life],
+
+    "6" : [gwarp,       crystal,    crystal2,   fuel,       crystal2,
+           crystal2,    fuel,       fuel,       crystal2,   fuel,
+           points2,     points2,    radio,      radio,      radio,
+           radio,       fueltank,   warp,       warp,       warp,
+           warp,        life,       life,       life],
+
+    "7" : [gwarp,       crystal2,   crystal2,   fuel,       crystal2,
+           fuel,        crystal2,   fuel,       crystal2,   fuel,
+           crystal,     warp,       warp,       points2,    points2,
+           radio,       radio,      fueltank,   fueltank,   warp,
+           life,        fueltank,   life,       life],
+
+    "8" : [minigame,    gwarp,      crystal2,   crystal2,   fuel,
+           crystal,     fuel,       fuel,       fuel,       crystal2,
+           radio,       points2,    radio,      radio,      warp,
+           warp,        fueltank,   warp,       fueltank,   fueltank,
+           life,        life],
+
+    "9" : [gwarp,       minigame,   fuel,       crystal2,   fuel,
+           crystal2,    fuel,       crystal2,   fuel,       fuel,
+           crystal,     points2,    points2,    radio,      points2,
+           warp,        fueltank,   fueltank,   warp,       warp,
+           fueltank,    life],
+
+    "10": [gwarp,       crystal2,   crystal,    fuel,       fuel,
+           crystal2,    fuel,       fuel,       crystal2,   fuel,
+           crystal2,    points2,    points2,    radio,      points2,
+           life,        warp,       fueltank,   life,       warp],
+
+    "11": [gwarp,       crystal2,   crystal2,   fuel,       fuel,
+           crystal,     crystal2,   fuel,       crystal2,   fuel,
+           crystal2,    points2,    points2,    radio,      points2,
+           radio,       warp,       fueltank,   warp,       warp,
+           fueltank,    fueltank,   life],
+
+    "12": [gwarp,       crystal2,   crystal2,   fuel,       crystal2,
+           crystal2,    fuel,       crystal,    fuel,       fuel,
+           crystal2,    points2,    points2,    radio,      points2,
+           points2,     warp,       warp,       fueltank,   warp,
+           warp,        fueltank,   life,       life],
+
+    "13": [gwarp,       crystal2,   fuel,       crystal,    crystal2,
+           fuel,        fuel,       crystal2,   fuel,       points2,
+           points2,     radio,      radio,      fueltank,   warp,
+           warp,        warp,       life,       life]
 }
 
 """
